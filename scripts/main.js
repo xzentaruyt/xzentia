8146e0b14611703939956e86babcf32334929c35
xzentaruyt <bruh.gd.play4gd@gmail.com>
2026-09-12 12:54:59 -0500

    fix 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 Events.on(ClientLoadEvent, () => {
-    const.sparrusTeam = Team.get(8); 
+    const sparrusTeam = Team.get(8); 
     sparrusTeam.name = "sparrus-faction";
     sparrusTeam.color = Color.valueOf("0079bf"); 
     sparrusTeam.hasCore = true;
