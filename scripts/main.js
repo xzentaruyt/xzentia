aab8303507f57dbff1a7b67fbb8d17d3dd7a4750
xzentaruyt <bruh.gd.play4gd@gmail.com>
2026-09-12 12:58:04 -0500

    removed java script because it brough pain

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-Events.on(ClientLoadEvent, () => {
-    const sparrusTeam = Team.get(8); 
-    sparrusTeam.name = "sparrus-faction";
-    sparrusTeam.color = Color.valueOf("0079bf"); 
-    sparrusTeam.hasCore = true;
-});
