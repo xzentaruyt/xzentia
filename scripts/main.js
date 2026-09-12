464b577fd8cca6a6861437cb9a685c2f1fff4dc4
xzentaruyt <bruh.gd.play4gd@gmail.com>
2026-09-12 12:52:26 -0500

    attempt 1added sparrow sprites

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+Events.on(ClientLoadEvent, () => {
+    global.sparrusTeam = Team.get(8); 
+    sparrusTeam.name = "sparrus-faction";
+    sparrusTeam.color = Color.valueOf("0079bf"); 
+    sparrusTeam.hasCore = true;
+});
